6c01421dabe0d41549c154a452eb57cb8882748f
stanovikov <stanovikov@ozon.ru>
2022-02-16 10:22:00 +0500

    Добавлены компоненты страниц

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,19 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../main-page/main-page';
+
+type MainPageProps = {
+  title: string,
+  genre: string,
+  year: number
+}
+
+function App({title, genre, year}: MainPageProps): JSX.Element {
+  return (
+    <MainPage
+      title={title}
+      genre={genre}
+      year={year}
+    />
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const mainPageProps = {
+  title: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  year: 2014,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      title={mainPageProps.title}
+      genre={mainPageProps.genre}
+      year={mainPageProps.year}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
