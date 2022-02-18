d0d5f2e4a22f03678868213481b2a3995cd55405
stanovikov <stanovikov@ozon.ru>
2022-02-18 16:59:48 +0500

    Домашка по роутингу

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,13 @@
-import MainPage from '../main-page/main-page';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../const';
+import SignIn from '../../pages/sign-in/sign-in-page';
+import MyList from '../../pages/my-list/my-list-page';
+import Film from '../../pages/film/film-page';
+import Player from '../../pages/player/player-page';
+import AddReviewPage from '../../pages/add-review/add-review-page';
+import NotFoundPage from '../../pages/404/404-page';
+import MainPage from '../../pages/main-page/main-page';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
   title: string,
@@ -8,11 +17,42 @@ type AppProps = {
 
 function App({title, genre, year}: AppProps): JSX.Element {
   return (
-    <MainPage
-      title={title}
-      genre={genre}
-      year={year}
-    />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainPage title={title} genre={genre} year={year} />}
+        />
+        <Route
+          path={AppRoute.SignIn}
+          element={<SignIn/>}
+        />
+        <Route
+          path={AppRoute.MyList}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <MyList/>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={`${AppRoute.Film}/:id`}
+          element={<Film/>}
+        />
+        <Route
+          path={AppRoute.AddReview}
+          element={`${<AddReviewPage/>}/:id/review`}
+        />
+        <Route
+          path={AppRoute.Player}
+          element={`${<Player/>}/:id`}
+        />
+        <Route
+          path="*"
+          element={<NotFoundPage/>}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/const.ts
@@ -0,0 +1,14 @@
+export enum AppRoute {
+  Root = '/',
+  AddReview = '/add-review',
+  Film = '/films',
+  MyList = 'my-list',
+  Player = '/player',
+  SignIn = '/sign-in'
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
